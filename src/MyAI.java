609f2fcd98abc3486b28cf6b2f2eaafd5e9f7e93
Lidya Stephanie Sentosa <lsentosa@uci.edu>
2019-07-06 13:40:20 -0700

    New uncoverZero function

--- src/MyAI.java
+++ src/MyAI.java
@@ -51,4 +51,15 @@ public class MyAI extends AI {
 
 	// ################### Helper Functions Go Here (optional) ##################
 	// ...
+
+	public void uncoverZero(int x, int y){
+		for(int i = -1;i < 1;i++){
+			for(int j = -1;j < 1;j++){
+				if(i == 0 && j == 0)
+					continue;
+				else
+
+			}
+		}
+	}
 }
