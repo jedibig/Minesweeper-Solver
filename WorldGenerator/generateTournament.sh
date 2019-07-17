c1653b7b7e58c2721449386912d6de1d8a801a9b
jedibig <jedidiahbowo@yahoo.com>
2019-07-16 23:27:51 -0700

    hu

--- WorldGenerator/generateTournament.sh
+++ WorldGenerator/generateTournament.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-rm -rf Problems
-mkdir Problems
+rm -rf Problems/Tournament
+mkdir Problems/Tournament
  
-python3 WorldGenerator.py 1000 Beginner_world_ 8 8 10
+python3 WorldGenerator.py 1000 Beginner_world_ Problems/Tournament 8 8 10
 
-python3 WorldGenerator.py 1000 Intermediate_world_ 16 16 40
+python3 WorldGenerator.py 1000 Intermediate_world_ Problems/Tournament 16 16 40
 
-python3 WorldGenerator.py 1000 Expert_world_ 16 30 99
+python3 WorldGenerator.py 1000 Expert_world_ Problems/Tournament 16 30 99
+
+java -jar ../bin/mine.jar -f Problems/Tournament ../scores/score_t.txt
 
 echo Finished generating worlds!
